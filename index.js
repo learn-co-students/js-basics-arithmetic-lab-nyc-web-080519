e7443075a353d6013b7e1df21e9496bca4fe56e5
Rutger <rutgerjmckenna@gmail.com>
2019-09-14 12:10:52 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here
+const var1 = 62;
+const var2 = 1;
+
+const multiply = var1 * var2;
+
+let random = Math.random();
+
+const mod = 16 / 4;
+
+const max = Math.max(1, 3, 9, 20)
