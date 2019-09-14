877d3444427dbd735ebc4e00ade3112c25ae2eef
kgriswo1 <kgriswo1@binghamton.edu>
2019-09-14 17:02:04 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here
+let var1 = 1;
+let var2 = 62
+
+let multiply = var1 * var2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let mod = 16 % 6;
+
+let max = Math.max(1, 2, 3, 20);
