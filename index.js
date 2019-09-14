a87ef1ed9c018c34add495e59f0ab4c576ce8c4a
Tracie Masek <42948851+traciemasek@users.noreply.github.com>
2019-09-14 10:52:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here
+let var1 = 1;
+let var2 = 62;
+let multiply = var1 * var2;
+
+const random = Math.ceil(Math.random() * 10);
+
+const mod = 16 % 6;
+
+const max = Math.max(1, 2, 3, 20)
